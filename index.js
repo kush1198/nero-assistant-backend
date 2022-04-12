29e05292dfa21d94da1a59550109f0ff6f26732c
Kushagra Bhatia <kushagra1198@gmail.com>
2022-04-12 01:21:16 -0700

    added login handler

--- index.js
+++ index.js
@@ -34,6 +34,21 @@ app.post("/register", async (req, res) => {
     });
   });
 
+  app.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username }).exec();
+    if (!user || user.password !== password) {
+      res.status(403);
+      res.json({
+        message: "invalid login",
+      });
+      return;
+    }
+    res.json({
+      message: "success",
+    });
+  });
+
 app.listen(port,() => {
     console.log('Listening')
 });
