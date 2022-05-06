b2b61f0233cfaf819f9b816dbaa4a9b0c1fd7624
Kushagra Bhatia <kushagra1198@gmail.com>
2022-05-06 14:04:28 -0700

    added License

--- index.js
+++ index.js
@@ -18,6 +18,17 @@ const userSchema = new mongoose.Schema({
   });
 const User = mongoose.model("User", userSchema);
 
+const todosSchema = new mongoose.Schema({
+  userId: mongoose.Schema.ObjectId,
+  todos: [
+    {
+      text: String,
+      id: String,
+    },
+  ],
+});
+const Todos = mongoose.model("Todos", todosSchema);
+
 app.post("/register", async (req, res) => {
     const { username, password } = req.body;
     const user = await User.findOne({ username }).exec();
@@ -48,7 +59,49 @@ app.post("/register", async (req, res) => {
       message: "success",
     });
   });
-
+  
+  app.post("/todos", async (req, res) => {
+    const { authorization } = req.headers;
+    const [, token] = authorization.split(" ");
+    const [username, password] = token.split(":");
+    const todosItems = req.body;
+    const user = await User.findOne({ username }).exec(); // common for all post and get requests
+    if (!user || user.password !== password) {
+      res.status(403);
+      res.json({
+        message: "invalid access",
+      });
+      return;
+    }
+    const todosExist = await Todos.findOne({ userId: user._id }).exec();
+    if (!todosExist) {
+      await Todos.create({
+        userId: user._id,
+        todos: todosItems,
+      });
+    } else {
+      todosExist.todos = todosItems;
+      await todosExist.save({ validateBeforeSave: false });
+    }
+    res.json(todosItems);
+  });
+  
+  app.get("/todos", async (req, res) => {
+    const { authorization } = req.headers;
+    const [, token] = authorization.split(" ");
+    const [username, password] = token.split(":");
+    const user = await User.findOne({ username }).exec();
+    if (!user || user.password !== password) {
+      res.status(403);
+      res.json({
+        message: "invalid access",
+      });
+      return;
+    }
+    const allTodos = await Todos.findOne({ userId: user._id }).exec();
+    if (!allTodos) return;
+    res.json(allTodos.todos);
+  });
 app.listen(port,() => {
     console.log('Listening')
 });
